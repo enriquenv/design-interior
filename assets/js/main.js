97267cc9282ad3dfbba5f60bd3b37c69b0770a3d
enriquenv <enriquen@live.de>
2021-07-27 15:56:29 +0200

    erste commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function log(){
+    console.log(document.getElementById("name").value);
+    console.log(document.getElementById("wmail").value);
+    console.log(document.getElementById("message").value);
+
+}
