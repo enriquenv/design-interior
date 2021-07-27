7397deb7b8b694137243ee91cb4dbcc2cc58fff9
enriquenv <enriquen@live.de>
2021-07-27 18:19:04 +0200

    zweite

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function log(){
     console.log(document.getElementById("name").value);
-    console.log(document.getElementById("wmail").value);
+    console.log(document.getElementById("email").value);
     console.log(document.getElementById("message").value);
 
 }
